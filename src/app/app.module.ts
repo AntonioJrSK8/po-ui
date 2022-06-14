971e76ed99aed4a06bc7afd9fcf5ee2972f98db2
AntonioJrSK8 <jr.sistema@gmail.com>
2022-06-14 19:26:05 -0300

    usando Diretiva inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { PoTemplatesModule } from '@po-ui/ng-templates';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeNewComponent } from './employee-new/employee-new.component';
 import { FormsModule } from '@angular/forms';
+import { SalaryColorDirective } from './diretiva/salary-color.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    EmployeeNewComponent
+    EmployeeNewComponent,
+    SalaryColorDirective
   ],
   imports: [
     BrowserModule,
